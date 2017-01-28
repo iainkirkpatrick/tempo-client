5a3834e4fbbf72ca89ca335b6ce9875d460f5add
Iain Kirkpatrick <kirkpatrick.iain@gmail.com>
2017-01-29 10:53:16 +1300

    prep index for main domain

--- app/index.js
+++ app/index.js
@@ -1,7 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+
+import store from './store'
+
+import MainContainer from './main/containers/main'
 
 ReactDOM.render(
-  <h1>TEMPO CLIENT</h1>,
+  <Provider store={store}>
+    <MainContainer />
+  </Provider>,
   document.body
 )
