da65dbd99f4844123bfc0634a81e63a98da93958
Iain Kirkpatrick <kirkpatrick.iain@gmail.com>
2017-01-29 16:00:48 +1300

    set up stilr for styles, wire up

--- app/index.js
+++ app/index.js
@@ -1,14 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
+import StyleSheet from 'stilr'
 
 import store from './store'
 
 import MainContainer from './main/containers/main'
 
+// create stylesheet for stilr
+let stylesheet = document.createElement('style')
+stylesheet.textContent = StyleSheet.render()
+document.head.appendChild(stylesheet)
+
+// create <app /> to mount into
+const mount = document.createElement('app')
+document.body.appendChild(mount)
+
 ReactDOM.render(
   <Provider store={store}>
     <MainContainer />
   </Provider>,
-  document.body
+  document.body.querySelector('app')
 )
--- app/main/containers/main.js
+++ app/main/containers/main.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
+import StyleSheet from 'stilr'
+
+const styles = StyleSheet.create({
+  header: {
+    fontSize: 18,
+    color: '#000'
+  }
+})
+
 class MainContainer extends React.Component {
   render () {
-    return <h1>Hello {this.props.main.dummyAppName}</h1>
+    return <h1 className={styles.header}>Hello {this.props.main.dummyAppName}</h1>
   }
 }
 
